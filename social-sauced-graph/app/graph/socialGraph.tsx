0b28ccfa48ab307c6c500ef570b09c0157282440
Noah Kruse <noahkruse@Noahs-MacBook-Pro.local>
2024-01-12 13:01:05 -0800

    update bubble size based on number of connections

--- social-sauced-graph/app/graph/socialGraph.tsx
+++ social-sauced-graph/app/graph/socialGraph.tsx
@@ -8,11 +8,11 @@ export default function SocialGraph() {
 
   const graph = {
     nodes: [
-      { id: 1, label: "Node 1", title: "node 1 tooltip text" },
-      { id: 2, label: "Node 2", title: "node 2 tooltip text" },
-      { id: 3, label: "Node 3", title: "node 3 tooltip text" },
-      { id: 4, label: "Node 4", title: "node 4 tooltip text" },
-      { id: 5, label: "Node 5", title: "node 5 tooltip text" }
+      { id: 1, size: 0, image: "https://avatars.githubusercontent.com/u/237133?v=4", shape: "circularImage", title: "node 1 tooltip text" },
+      { id: 2, size: 0, image: "https://avatars.githubusercontent.com/u/237133?v=4", shape: "circularImage", title: "node 2 tooltip text" },
+      { id: 3, size: 0, image: "https://avatars.githubusercontent.com/u/237133?v=4", shape: "circularImage", title: "node 3 tooltip text" },
+      { id: 4, size: 0, image: "https://avatars.githubusercontent.com/u/237133?v=4", shape: "circularImage", title: "node 4 tooltip text" },
+      { id: 5, size: 0, image: "https://avatars.githubusercontent.com/u/237133?v=4", shape: "circularImage", title: "node 5 tooltip text" }
     ],
     edges: [
       { from: 1, to: 2 },
@@ -21,6 +21,37 @@ export default function SocialGraph() {
       { from: 2, to: 5 }
     ]
   };
+
+
+  const connectionBubbleSize = (id: number) => {
+    const graphEdges = graph.edges.length;
+    let counter = 1;
+    for (let i = 0; i < graphEdges; i++) {
+      if (graph.edges[i].from == id || graph.edges[i].to == id) {
+        counter++;
+      }
+    }
+    return counter * 10;
+  }
+
+  //post process
+  // const populateBubbleSize = () => {
+  //   const nodes = graph.nodes.length;
+  //   let counter = 0;
+  //   for (let i = 0; i < nodes; i++) {
+  //     // if (graph.edges[i].from == id) {
+  //     //   counter++;
+  //     // }
+  //     graph.nodes[i].size = connectionBubbleSize(graph.nodes[i].id)
+  //   }
+  // }
+
+  // populateBubbleSize();
+
+  // Calculate and assign sizes to nodes
+  graph.nodes.forEach(node => {
+    node.size = connectionBubbleSize(node.id);
+  });
 
   const options = {
     layout: {
